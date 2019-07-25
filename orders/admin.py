7d027a7ba13d9b0bdf02b70d33368db93edb967a
Jefferson Maldonado <jeffmaldo27@gmail.com>
2019-07-24 23:11:15 -0500

    models updated

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import entree, size, category, addon, addonLimits
+
+admin.site.register(entree)
+admin.site.register(size)
+admin.site.register(category)
+admin.site.register(addon)
+admin.site.register(addonLimits)
+
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class size(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class addon(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class addonLimits(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class entree(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.DecimalField(help_text="Price in $US", max_digits=4, decimal_places=2)
+    size = models.ForeignKey(size, on_delete=models.CASCADE)
+    category = models.ForeignKey(category, on_delete=models.CASCADE)
+    addonLimits = models.ForeignKey(addonLimits, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.size} {self.name} {self.category} - {self.price}"
